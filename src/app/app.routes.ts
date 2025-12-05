a550fe62d83bf2cfe338ac6e41d80a5289c203d7
Emm Kall <ing.emmanuel.cal@gmail.com>
2025-12-05 10:20:36 -0600

    [feature] clean base to start

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,28 +2,17 @@ import { Routes } from '@angular/router';
 import { App } from './app';
 
 export const routes: Routes = [
-  {
+  /* {
     path: 'dashboard',
     //loadComponent: () => import('./gifs/pages/dashboard/dashboard').then( (m) => m.Dashboard ), },
-    loadComponent: () => import('./gifs/pages/dashboard/dashboard'),
+    //loadComponent: () => import('./gifs/pages/dashboard/dashboard'),
     children: [
       {
         path: 'trending',
         loadComponent: () => import('./gifs/pages/trending/trending'),
       },
-      {
-        path: 'search',
-        loadComponent: () => import('./gifs/pages/search/search')
-      },
-      {
-        path: 'history/:query',
-        loadComponent: () => import('./gifs/pages/gif-history/gif-history')
-      },
-      {
-        path: '**',
-        redirectTo: 'trending',
-      },
     ]
-  },
-  { path: '**', redirectTo: 'dashboard', pathMatch: 'full' },
+  }, */
+  { path: '', component: App, pathMatch: 'full' },
+  { path: '**', redirectTo: '/', pathMatch: 'full' },
 ];
